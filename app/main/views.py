4b93e6694fdced0848be6b54c9cda0c62a0f6754
Likit Preeyanon <likit.pre@mahidol.edu>
2019-03-12 23:09:35 +0700

    added account related views and templates
    updated the menu bar

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,9 @@ from flask import render_template, redirect, request, url_for
 from . import mainbp as main
 from .forms import SignUpForm, LogInForm
 from collections import defaultdict
-from .models import User
+from .models import User, Project
 from ..app import db
-from flask_login import login_user, current_user, logout_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -39,7 +39,7 @@ def log_user_in():
         if user:
             pw = request.form.get('password')
             if pw == user.password:
-                login_user(user)
+                login_user(user, remember=True)
                 return redirect('/')
             else:
                 error_msg = 'Password is not correct.'
@@ -51,6 +51,7 @@ def log_user_in():
 
 
 @main.route('/logout_form', methods=['GET', 'POST'])
+@login_required
 def log_user_out():
     if current_user.is_authenticated:
         form = LogInForm()
@@ -58,3 +59,30 @@ def log_user_out():
         return render_template('main/logout.html', form=form)
     else:
         return redirect(url_for('main.log_user_in'))
+
+
+@main.route('/account/main')
+@login_required
+def account_dash():
+    projects = {'pending': 0, 'approved': 0, 'total': 0}
+    affil_researchers = []
+    affil_projects = {'pending': 0, 'approved': 0, 'total': 0}
+    for researcher in current_user.profile.affil.affiliates:
+        for proj in researcher.user.created_projects:
+            if proj.status.status == 'pending':
+                affil_projects['pending'] += 1
+            elif proj.status.status == 'approved':
+                affil_projects['approved'] += 1
+        affil_projects['total'] += 1
+        affil_researchers.append(researcher)
+
+    for proj in current_user.created_projects:
+        if proj.status.status == 'pending':
+            projects['pending'] += 1
+        elif proj.status.status == 'approved':
+            projects['approved'] += 1
+        projects['total'] += 1
+    return render_template('main/account_dash.html',
+                           projects=projects,
+                           affil_projects=affil_projects,
+                           affil_researchers=affil_researchers)
